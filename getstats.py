6acfac7abb456ee97cbd78dc66cd4815005c0043
Tobias Bastable <tobiasbastable@gmail.com>
2021-02-16 19:56:46 +0000

    Adds TrackerAPI class to getstats for handling API request
    TrackerAPI class separated handling of API request and response from main PlayerStats class, and records response errors.

--- getstats.py
+++ getstats.py
@@ -3,6 +3,7 @@ import requests
 
 class PlayerStats:
     def __init__(self,
+                 raw_response_data,
                  platform=None,
                  username=None,
                  season_name=None,
@@ -32,41 +33,29 @@ class PlayerStats:
         self.saves = saves
         self.shots = shots
         self.assists = assists
+        self.raw_response_data = raw_response_data
+        self.extract_player_stats_from_response()
 
-    @staticmethod
-    def generate_response(username, platform):
-        url = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/{platform}/{username}'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/86.0.4240.198 Safari/537.36'}
-        response = requests.get(url, headers=headers)
-        if not response.ok:
-            print(f"API failed with a {response.status_code} response - aborting")
-            return
-        raw_response_data = response.json().get("data", {})
-        return raw_response_data
-
-    def extract_player_stats_from_response(self, raw_response_data):
+    def extract_player_stats_from_response(self):
         platform_dic = {'xbl': 'Xbox Live', 'psn': 'Playstation Network', 'steam': 'Steam'}
-        self.platform = platform_dic[raw_response_data['platformInfo']['platformSlug']]
-        self.username = raw_response_data
-        self.username = raw_response_data['platformInfo']['platformUserHandle']
-        self.season_name = raw_response_data['metadata']['currentSeason']
-        self.lifetime_wins = raw_response_data['segments'][0]['stats']['wins']['displayValue']
-        self.mvps = raw_response_data['segments'][0]['stats']['mVPs']['displayValue']
-        self.goals = raw_response_data['segments'][0]['stats']['goals']['displayValue']
-        self.saves = raw_response_data['segments'][0]['stats']['saves']['displayValue']
-        self.shots = raw_response_data['segments'][0]['stats']['shots']['displayValue']
-        self.assists = raw_response_data['segments'][0]['stats']['assists']['displayValue']
-        self.lifetime_win_rank = raw_response_data['segments'][0]['stats']['wins']['rank']
-        self.goal_shot_ratio = raw_response_data['segments'][0]['stats']['goalShotRatio']['displayValue']
-        self.season_reward = raw_response_data['segments'][0]['stats']['seasonRewardLevel']['metadata'][
+        self.platform = platform_dic[self.raw_response_data['platformInfo']['platformSlug']]
+        self.username = self.raw_response_data['platformInfo']['platformUserHandle']
+        self.season_name = self.raw_response_data['metadata']['currentSeason']
+        self.lifetime_wins = self.raw_response_data['segments'][0]['stats']['wins']['displayValue']
+        self.mvps = self.raw_response_data['segments'][0]['stats']['mVPs']['displayValue']
+        self.goals = self.raw_response_data['segments'][0]['stats']['goals']['displayValue']
+        self.saves = self.raw_response_data['segments'][0]['stats']['saves']['displayValue']
+        self.shots = self.raw_response_data['segments'][0]['stats']['shots']['displayValue']
+        self.assists = self.raw_response_data['segments'][0]['stats']['assists']['displayValue']
+        self.lifetime_win_rank = self.raw_response_data['segments'][0]['stats']['wins']['rank']
+        self.goal_shot_ratio = self.raw_response_data['segments'][0]['stats']['goalShotRatio']['displayValue']
+        self.season_reward = self.raw_response_data['segments'][0]['stats']['seasonRewardLevel']['metadata'][
             'rankName']
         match_dic = {}
         for i in range(2, 10):
-            match = raw_response_data['segments'][i]['stats']['matchesPlayed']['value']
-            name = raw_response_data['segments'][i]['metadata']['name']
-            rank = raw_response_data['segments'][i]['metadata']['name']
+            match = self.raw_response_data['segments'][i]['stats']['matchesPlayed']['value']
+            name = self.raw_response_data['segments'][i]['metadata']['name']
+            rank = self.raw_response_data['segments'][i]['metadata']['name']
             match_dic[name] = (match, rank)
         self.match_info = match_dic
         return
@@ -80,3 +69,35 @@ class PlayerStats:
               f"Lifetime wins: {self.lifetime_wins}\n"
               f"Lifetime win rank: {self.lifetime_win_rank}\n"
               f"Lifetime goal to shot ratio: {self.goal_shot_ratio}%")
+
+
+class TrackerAPI:
+    def __init__(self, platform, username):
+        self.platform = platform
+        self.user = str.replace(username, " ", "%20")
+        self.raw_response_data = None
+        self.response = None
+        self.get_response()
+
+    def get_response(self):
+        url = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/{self.platform}/{self.user}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/86.0.4240.198 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+        if not response.ok:
+            self.response = False
+        else:
+            self.response = True
+        raw_response_data = response.json().get("data", {})
+        self.raw_response_data = raw_response_data
+        return
+
+
+
+response = TrackerAPI('xbl', "UKF WONDERBOY2")
+
+print(response.response)
+
+if not response.response:
+    print(f'Error receiving data for {response.user}. Please check spelling and platform and try again.')
