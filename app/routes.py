112ba2fee7f131749630861d14b7a0c248c939f3
Tobias Bastable <tobiasbastable@gmail.com>
2021-02-16 19:59:43 +0000

    Updates routes for use with TrackerAPI class in getstats
    routes now uses TrackerAPI and PlayerStats classes. Causes flashed messages when incorrect username or platform combo are entered.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
-from flask import render_template, request, flash, Flask, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for
 from app.forms import UsernameForm
-from getstats import PlayerStats
+from getstats import PlayerStats, TrackerAPI
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,24 +9,24 @@ from getstats import PlayerStats
 def index():
     form = UsernameForm()
     if request.method == 'POST':
-            return redirect(url_for('compare'))
+        return redirect(url_for('compare'))
     return render_template('index.html', title='Rocket League Stats', form=form)
 
 
 @app.route('/compare', methods=['GET'])
 def compare():
-    user1 = str.replace(request.args.get("username")," ","%20")
-    user2 = str.replace(request.args.get("username2")," ","%20")
-    platform1 = request.args.get("platform")
-    platform2 = request.args.get("platform2")
+    response1 = TrackerAPI(request.args.get("platform"), request.args.get("username"))
+    response2 = TrackerAPI(request.args.get("platform2"), request.args.get("username2"))
+    if not response1.response or response2.response:
+        if not response1.response:
+            flash(f'Error receiving data for {request.args.get("username")}.'
+                  f' Please check spelling and platform and try again.')
+        if not response2.response:
+            flash(f'Error receiving data for {request.args.get("username2")}. '
+                  f'Please check spelling and platform and try again.')
+        return redirect(url_for('index'))
 
-    account1 = PlayerStats()
-    player_stats1 = account1.generate_response(user1, platform1)
-    account1.extract_player_stats_from_response(player_stats1)
-    account2 = PlayerStats()
-    player_stats2 = account1.generate_response(user2, platform2)
-    account2.extract_player_stats_from_response(player_stats2)
+    user1 = PlayerStats(response1.raw_response_data)
+    user2 = PlayerStats(response2.raw_response_data)
 
-    return render_template('compare.html', player1=account1, player2=account2)
-
-
+    return render_template('compare.html', player1=user1, player2=user2)
