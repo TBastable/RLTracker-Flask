03dbb91101bd983f9edcc44c5003324372f2bdbd
Tobias Bastable <tobiasbastable@gmail.com>
2021-02-21 12:15:08 +0000

    Changes compare to align with changes to TrackerAPI, creates flash error message function

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request, flash, redirect, url_for
 from app.forms import UsernameForm
 from getstats import PlayerStats, TrackerAPI
+import requests
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,20 +14,26 @@ def index():
     return render_template('index.html', title='Rocket League Stats', form=form)
 
 
-@app.route('/compare', methods=['GET'])
-def compare():
-    response1 = TrackerAPI(request.args.get("platform"), request.args.get("username"))
-    response2 = TrackerAPI(request.args.get("platform2"), request.args.get("username2"))
-    if not response1.response or response2.response:
-        if not response1.response:
-            flash(f'Error receiving data for {request.args.get("username")}.'
-                  f' Please check spelling and platform and try again.')
-        if not response2.response:
-            flash(f'Error receiving data for {request.args.get("username2")}. '
-                  f'Please check spelling and platform and try again.')
+def get_user_profile_with_flash_error(username, platform):
+    try:
+        return TrackerAPI.get_user_profile_data(platform, username)
+    except requests.exceptions.HTTPError:
+        flash(f'Error receiving data for {username}. '
+              f'Please check spelling and platform and try again.')
         return redirect(url_for('index'))
 
-    user1 = PlayerStats(response1.raw_response_data)
-    user2 = PlayerStats(response2.raw_response_data)
 
-    return render_template('compare.html', player1=user1, player2=user2)
+@app.route('/compare', methods=['GET'])
+def compare():
+    username_1 = request.args.get("username")
+    username_1_platform = request.args.get("platform")
+    user_1_profile_data = get_user_profile_with_flash_error(platform=username_1_platform, username=username_1)
+
+    username_2 = request.args.get("username2")
+    username_2_platform = request.args.get("platform2")
+    user_2_profile_data = get_user_profile_with_flash_error(platform=username_2_platform, username=username_2)
+
+    user_1_stats = PlayerStats(user_1_profile_data)
+    user_2_stats = PlayerStats(user_2_profile_data)
+
+    return render_template('compare.html', player1=user_1_stats, player2=user_2_stats)
